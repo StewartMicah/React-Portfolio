9b0d342704e520e344c3fc2800df1536dff02a26
Beepo <micahstew@gmail.com>
2022-07-23 13:22:56 -0400

    adding pngs and formatting

---
+++ src/components/Footer/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="flex-row foot">
+      <div className="flex-row">
+        <header className="flex-row px-1">
+          <a className="github" href="https://github.com/StewartMicah"></a>
+        </header>
+        <header className="flex-row px-1">
+          <a
+            className="stackoverflow"
+            href="https://stackoverflow.com/users/18003755/beepo"
+          ></a>
+        </header>
+        <header className="flex-row px-1">
+          <a
+            className="linkedin"
+            href="https://www.linkedin.com/in/micah-stewart-84911822b/"
+          ></a>
+        </header>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/pages/Portfolio.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Portfolio = () => {
+  return (
+    <div className="container">
+      <h1>Portfolio</h1>
+      <div className="projects">
+        <div className="card">
+          <div className="thumbnail"></div>Card Game
+        </div>
+        <div className="card">
+          <div className="thumbnail"></div>Test
+        </div>
+        <div className="card">
+          <div className="thumbnail"></div>Test
+        </div>
+      </div>
+      <div className="projects">
+        <div className="card">
+          <div className="thumbnail"></div>Test
+        </div>
+        <div className="card">
+          <div className="thumbnail"></div>Test
+        </div>
+        <div className="card">
+          <div className="thumbnail"></div>Test
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Portfolio;
