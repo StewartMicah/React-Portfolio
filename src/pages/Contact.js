fcb643aa833b6199cf2720c01f0c3198abb58da8
Beepo <micahstew@gmail.com>
2022-07-26 15:41:09 -0400

    working app

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,62 +1,66 @@
-import React from "react";
+import React, { useState } from 'react';
 
-const Contact = () => {
+import { validateEmail } from '../Utils/Helpers';
+
+function Contact() {
+  const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+
+  const [errorMessage, setErrorMessage] = useState('');
+  const { name, email, message } = formState;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!errorMessage) {
+      console.log('Submit Form', formState);
+    }
+  };
+
+  const handleChange = (e) => {
+    if (e.target.name === 'email') {
+      const isValid = validateEmail(e.target.value);
+      if (!isValid) {
+        setErrorMessage('Your email is invalid.');
+      } else {
+        setErrorMessage('');
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage('');
+      }
+    }
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+      console.log('Handle Form', formState);
+    }
+  };
+
   return (
-    <div className="container">
-      <h1>Contact</h1>
-      <div>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ornare
-        blandit vestibulum. Nulla interdum mattis tortor, ac dignissim justo
-        dapibus nec. Integer tincidunt, massa quis sagittis tincidunt, lacus
-        arcu varius nisl, eget porta purus arcu quis eros. Duis semper nisi quis
-        massa pharetra faucibus. In sed dui est. Integer risus quam, cursus sed
-        condimentum nec, sodales non eros. Sed vehicula elit tortor, vel
-        lobortis lectus consectetur quis. Pellentesque malesuada, felis suscipit
-        ornare mollis, neque tellus sollicitudin orci, ultricies iaculis tortor
-        enim vehicula libero. Aenean feugiat, enim in pellentesque imperdiet,
-        orci lacus gravida turpis, ut gravida ante ante et urna. Nullam congue
-        maximus porttitor. Mauris fermentum volutpat dui, et mattis lectus
-        pellentesque quis. Quisque egestas nisl velit, id ultricies ipsum
-        lobortis eget. Curabitur pharetra gravida erat et facilisis. Suspendisse
-        eu turpis fringilla, consequat purus sed, venenatis erat. Curabitur
-        porttitor justo sapien, sed accumsan libero semper quis. In placerat
-        enim lectus, id vestibulum elit vulputate eu. Nullam non metus pulvinar,
-        laoreet erat sed, vehicula orci. Proin accumsan velit nec lacus lobortis
-        facilisis. Nunc dictum ligula et pellentesque aliquet. Pellentesque
-        habitant morbi tristique senectus et netus et malesuada fames ac turpis
-        egestas. Vestibulum auctor diam eu mi euismod molestie. Praesent id
-        dolor magna. Nullam varius odio est, vitae imperdiet erat cursus in.
-        Vestibulum facilisis sed turpis ut ultrices. Aliquam ut aliquam ex.
-        Quisque egestas pharetra felis, vitae tristique velit lacinia in.
-        Aliquam erat volutpat. Nam arcu ex, laoreet id dui a, tempor lacinia
-        orci. Praesent sollicitudin, nulla eget condimentum gravida, orci lorem
-        facilisis ante, sit amet gravida augue tortor a enim. Quisque at diam
-        maximus neque posuere condimentum. Cras id magna mauris. Donec et purus
-        mattis, sodales ex vel, convallis nunc. Mauris semper nunc a ex accumsan
-        imperdiet. Nullam elementum erat sapien, ac mattis orci mollis at. Donec
-        gravida maximus tellus. Praesent maximus ante vulputate pellentesque
-        luctus. Ut ut rutrum dui. Vivamus semper arcu posuere, feugiat libero
-        et, fermentum tortor. Pellentesque habitant morbi tristique senectus et
-        netus et malesuada fames ac turpis egestas. In sed tempor augue.
-        Suspendisse potenti. Morbi sagittis porttitor quam in hendrerit. Proin
-        blandit luctus feugiat. Proin euismod felis eget elit gravida porta.
-        Donec dignissim lobortis interdum. Integer elementum dapibus lacus at
-        blandit. Mauris elementum, sapien in scelerisque convallis, nulla libero
-        placerat sapien, non tincidunt magna velit id ligula. Integer laoreet
-        justo ante, ac varius nulla suscipit nec. Nulla nec dui laoreet nunc
-        pharetra vestibulum. Vivamus mattis felis sed orci condimentum mattis.
-        Donec lobortis, lacus ac hendrerit dignissim, erat turpis tincidunt
-        erat, vel tristique felis eros ac lectus. Mauris eu ultricies augue.
-        Quisque malesuada est tortor, quis tristique mi mollis vel. Quisque a
-        accumsan eros, dignissim faucibus nibh. Aliquam urna quam, auctor nec
-        pellentesque at, efficitur non nibh. Curabitur vitae vulputate quam, id
-        congue metus. Phasellus ac dui dictum, placerat urna eget, varius purus.
-        Donec eget elit vitae odio eleifend finibus eget sed turpis. Nam
-        vulputate leo sit amet lectus facilisis, non imperdiet lectus
-        scelerisque.
-      </div>
-    </div>
+    <section>
+      <h1 data-testid="h1tag">Contact me</h1>
+      <form id="contact-form" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
+        </div>
+        <div>
+          <label htmlFor="email">Email address:</label>
+          <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
+        </div>
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
+        <button data-testid="button" type="submit">Submit</button>
+      </form>
+    </section>
   );
-};
+}
 
 export default Contact;
